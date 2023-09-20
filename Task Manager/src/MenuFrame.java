d19d706b97a19170a8a474c136242836dab0d892
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2023-09-20 19:12:54 +0200

    making small progress!

---
+++ Task Manager/src/MenuFrame.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class MenuFrame extends JFrame {
+    private JPanel _mainPanel, _headerPanel, _menuPanel, _bottomPanel, _sortPanel;
+    private JLabel _title, _createTask, _showTasks;
+
+}
--- Task Manager/src/TaskManager.java
+++ Task Manager/src/TaskManager.java
@@ -1,7 +1,17 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class TaskManager {
     private ArrayList<Task> _tasks;
 
+    public TaskManager(){
+        super();
+
+    }
+
+    public void add_task(String task, Date endingDate, int priority){
+        _tasks.add(new Task(task, endingDate, priority));
+
+    }
 
 }
