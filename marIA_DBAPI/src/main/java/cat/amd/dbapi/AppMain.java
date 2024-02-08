0f57a4a22625fcc2a62b4c7a3bf903daaf819911
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2024-02-08 17:49:34 +0100

    implemented AppMain.java class main static method. Created and implemented persistance.SessionFactoryManager.java class

--- marIA_DBAPI/src/main/java/cat/amd/dbapi/AppMain.java
+++ marIA_DBAPI/src/main/java/cat/amd/dbapi/AppMain.java
@@ -1,11 +1,15 @@
 package cat.amd.dbapi;
 
+import cat.amd.dbapi.persistance.SessionFactoryManager;
+import org.apache.commons.cli.*;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
 
 public class AppMain {
@@ -40,5 +44,49 @@ public class AppMain {
      */
     public static void main(String[] args) {
         // TODO implement main
+
+        Options options = new Options();
+
+        Option hostOption = new Option("h", "host", true, "Host address. Default value: 127.0.0.1");
+        hostOption.setRequired(false);
+        options.addOption(hostOption);
+
+        Option portOption = new Option("p", "port", true, "Port. Default value: 8080");
+        hostOption.setRequired(false);
+        options.addOption(portOption);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+
+            String host = cmd.hasOption("host") ? cmd.getOptionValue("host") : "127.0.0.1";
+            int port = cmd.hasOption("port") ? Integer.parseInt(cmd.getOptionValue("port")) : 8080;
+
+            logger.info("Running hibernate...");
+            // runHibernate(); // TODO Hibernate implementation missing;
+
+            logger.info("Running server...");
+            final HttpServer server = runServer(host, port);
+
+            logger.info(String.format("App started with WADL available at " +
+                    "%sapplication.wadl\nPress \"Enter\" to stop it...", baseURI));
+            System.in.read();
+
+            SessionFactoryManager.close();
+            server.shutdownNow();
+
+        } catch (ParseException e) {
+            logger.error("Error while processing app parameters.", e);
+            formatter.printHelp("utility-name", options);
+            System.exit(1);
+
+        } catch (IOException e) {
+            logger.error("Error while trying to run the server.", e);
+            System.exit(1);
+        }
+
     }
 }
---
+++ marIA_DBAPI/src/main/java/cat/amd/dbapi/persistance/SessionFactoryManager.java
@@ -0,0 +1,47 @@
+package cat.amd.dbapi.persistance;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SessionFactoryManager {
+    private static final Logger logger = LoggerFactory.getLogger(SessionFactoryManager.class);
+    private static final SessionFactory factory;
+
+    static {
+        try {
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+
+            Metadata metadata = new MetadataSources(registry).buildMetadata();
+            factory = metadata.getSessionFactoryBuilder().build();
+        } catch (Throwable e) {
+            logger.error("Error on sessionFactory object creation.", e);
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    /**
+     * SessionFactory getter
+     *
+     * @return returns the current instance of SessionFactory
+     */
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+
+    /**
+     * If the current SessionFactory instance is not null, closes it.
+     *
+     */
+    public static void close() {
+        if (factory != null) {
+            factory.close();
+        }
+    }
+}
