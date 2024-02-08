917bd1a3f3773986cee16b6278b909264039861f
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2024-02-08 17:22:57 +0100

    added AppMain.java class method skeleton

--- marIA_DBAPI/src/main/java/cat/amd/dbapi/AppMain.java
+++ marIA_DBAPI/src/main/java/cat/amd/dbapi/AppMain.java
@@ -1,4 +1,39 @@
 package cat.amd.dbapi;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AppMain {
+    private static final Logger logger = LoggerFactory.getLogger(AppMain.class);
+    private static String baseURI; // maybe can be final
+
+    /**
+     * Sets up and runs hibernate
+     *
+     */
+    public static void runHibernate() {
+        // TODO implement hibernate
+    }
+
+    /**
+     * Runs the API server
+     *
+     * @param host
+     * @param port
+     * @return
+     */
+    public static HttpServer runServer(String host, int port) {
+        // TODO implement server start up
+        return null;
+    }
+
+    /**
+     * Main method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO implement main
+    }
 }
