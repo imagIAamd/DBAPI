438591756f1a9a88f7fcf64e07e1308141e60b51
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2024-02-08 17:28:32 +0100

    began runServer method implementation

--- marIA_DBAPI/src/main/java/cat/amd/dbapi/AppMain.java
+++ marIA_DBAPI/src/main/java/cat/amd/dbapi/AppMain.java
@@ -1,16 +1,19 @@
 package cat.amd.dbapi;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+
 public class AppMain {
     private static final Logger logger = LoggerFactory.getLogger(AppMain.class);
-    private static String baseURI; // maybe can be final
+    private static String baseURI;
 
     /**
      * Sets up and runs hibernate
-     *
      */
     public static void runHibernate() {
         // TODO implement hibernate
@@ -19,19 +22,21 @@ public class AppMain {
     /**
      * Runs the API server
      *
-     * @param host
-     * @param port
-     * @return
+     * @param host Server host
+     * @param port Server port
+     * @return instance of HttpServer
      */
     public static HttpServer runServer(String host, int port) {
-        // TODO implement server start up
-        return null;
+        baseURI = String.format("http://%s:%d/api/", host, port);
+        final ResourceConfig rc = new ResourceConfig().packages("cat.amd.dbapi");
+
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(baseURI), rc);
     }
 
     /**
      * Main method
      *
-     * @param args
+     * @param args Main method args
      */
     public static void main(String[] args) {
         // TODO implement main
