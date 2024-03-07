052d4fab951d35644fd6febb42fb8cc5d29761dd
achivaq01 <145363069+achivaq01@users.noreply.github.com>
2024-03-07 08:24:46 +0100

    xd

--- marIA_DBAPI/src/main/java/cat/amd/dbapi/endpoints/UserController.java
+++ marIA_DBAPI/src/main/java/cat/amd/dbapi/endpoints/UserController.java
@@ -67,14 +67,11 @@ public class UserController {
         }
 
         user = new User(requestJson);
-        user = UserManager.findUser(user);
         LOGGER.info("Received SMS code: {}", requestJson.get(VALIDATION_CODE));
-        if (user == null) {
+        user.setValidationCode(requestJson.getInt(VALIDATION_CODE));
+        if (UserManager.findUser(user) == null) {
             return CommonManager.buildBadRequestResponse();
         }
-
-        user.setValidationCode(requestJson.getInt(VALIDATION_CODE));
-        UserManager.updateUser(user);
 
         responseData.put(NICKNAME, user.getNickname())
                 .put(PHONE_NUMBER, user.getTelephone())
